588ab463c6fc6d35d6c227e4c63da4458213e9be
yuefang1991 <fangyue_1991@126.com>
2022-12-13 22:28:47 -0800

    init

---
+++ R/main.R
@@ -0,0 +1,7 @@
+
+#-- demo--
+# connection = prepare_table()
+# search_res = keyword_search(db = connection)
+# top10 = print_top_N_platform(db = connection)
+# barplot_top_N_platform(top10)
+# barplot_interact(top10)
